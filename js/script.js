009c61e7697d34d9da651cdad43707393d32fde5
Artem Vodyanitsky <artem.vodyanitsky@codeit.com.ua>
2017-08-29 13:19:27 +0300

    markup finished

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(function(){
-
 	$.getJSON( "js/TestData.json", function(data) {
 
 		var $programElement = $('#program');
+		var $tableBodyElement = $('#table');
 
 		data.Programs.forEach(function(item){
 			var $newItem = $('<div class="row program-item"></div>');
@@ -35,7 +35,39 @@ $(function(){
 					'');
 			}
 		});
-		console.log(data);
+
+		data.CurrentProgram.Members.forEach(function(item){
+			$tableBodyElement.append('' +
+				'<tr>' +
+					'<td>' +
+						'<a class="message" href="#" data-toggle="tooltip"></a>' +
+						'<div class="image-wrap">' +
+							'<img src="' + item.AvatarURL + '" alt="">' +
+						'</div>' +
+						'<div class="name">' + item.Name + '</div>' +
+						'<div class="phase">Phase: ' + item.Phase + '</div>' +
+						'<div class="age">' + item.Age + '</div>' +
+					'</td>' +
+					'<td>' +
+						'<div class="value">' + item.AvgBGPreMeal.Value + '</div>' +
+						'<div class="date">' + item.AvgBGPreMeal.Date + '</div>' +
+					'</td>' +
+					'<td>' + item.CriticalBGLogs + '</td>' +
+					'<td>' + item.LastClientMessage + '</td>' +
+					'<td>' + item.LastFoodPhoto + '</td>' +
+					'<td class="' + item.LastMoodReported.Color + '">' + item.LastMoodReported.Value + '</td>' +
+				'</tr>' +
+				'')
+		});
+
+		$('#members-number').text(data.CurrentProgram.Members.length);
+		$('#table-name').text(data.CurrentProgram.Name);
+
+		/*tooltip init*/
+		$('.message[data-toggle="tooltip"]').tooltip({
+			title: "Send message",
+			placement: "right",
+		});
 	});
 });
 
